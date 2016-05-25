5ca98073f617511a6a0ccc13268ca447cdbcd473
Thomas F <taforres@asu.edu>
2016-05-24 21:06:01 -0700

    Getter methods for days

--- src/main/java/mtgdata/Card.java
+++ src/main/java/mtgdata/Card.java
@@ -5,23 +5,67 @@ public class Card {
 	/**Card Stats */
 	private String name;
 	private String id;
-	private ArrayList colors;
-	private ArrayList types;
+	private ArrayList<String> colors;
+	private ArrayList<String> types;
 	private String cost;
 	private String cardText;
 	private int cmc;
 	
-	/**Formats */
+	/** Formats */
 	private boolean commanderLegal;
 	private boolean legacyLegal;
 	private boolean vintageLegal;
 	
-	/**Deckbrew urls  */
+	/** Deckbrew urls  */
 	private String urlDeckBrew;
 	private String storeUrlDeckBrew;
 	
 	public Card() {
 		
+	}
+	
+	public String getCardName() {
+		return name;
+	}
+	
+	public String getCardId() {
+		return id;
+	}
+	
+	public ArrayList<String> getCardColors() {
+		return colors;
+	}
+	
+	public ArrayList<String> getCardTypes() {
+		return types;
+	}
+	
+	public String getCardCost() {
+		return cost;
+	}
+	
+	public String getCardText(){
+		return cardText;
+	}
+	
+	public boolean getCommanderLegal() {
+		return commanderLegal;
+	}
+	
+	public boolean getLegacyLegal() {
+		return legacyLegal;
+	}
+	
+	public boolean vintageLegal() {
+		return vintageLegal;
+	}
+	
+	public String getUrlDeckBrew() {
+		return urlDeckBrew;
+	}
+	
+	public String getStoreUrl() {
+		return storeUrlDeckBrew;
 	}
 
 }
